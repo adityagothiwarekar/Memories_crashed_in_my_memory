ab9e26d2f269bbe83647148873528ffb4e3b4873
Aditya Gothiwarekar <aditya.gothiwarekar@gmail.com>
2024-04-25 13:20:09 -0500

    Create newpage.dart

---
+++ lib/newpage.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter_share/flutter_share.dart';
+import 'package:assignment_one/database.dart';
+import 'package:share_extend/share_extend.dart'; // Import Memory from database.dart
+
+class MemoriesPage extends StatelessWidget {
+  final List<Memory> memories;
+
+  const MemoriesPage({Key? key, required this.memories}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Memories'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the previous screen
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: memories.length,
+        itemBuilder: (context, index) {
+          return MemoryCard(memory: memories[index]);
+        },
+      ),
+    );
+  }
+}
+
+class MemoryCard extends StatelessWidget {
+  final Memory memory;
+
+  const MemoryCard({Key? key, required this.memory}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.file(File(memory.imagePath)),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(memory.city),
+          ),
+          SizedBox(height: 8.0),
+          ElevatedButton(
+            onPressed: () async {
+              await _shareMemory(memory.imagePath); // Pass imagePath to _shareMemory
+            },
+            child: Text('Share on WhatsApp'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _shareMemory(String imagePath) async {
+    try {
+      await ShareExtend.share(imagePath, "image"); // Share the image using share_extend
+    } catch (e) {
+      print('Error sharing memory: $e');
+    }
+  }
+}
